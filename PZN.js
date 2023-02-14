911b51aff2ae20d373cac1144b570ed5fe3133bb
xaunikmas <xaunikmas@gmail.com>
2023-02-14 10:35:23 +0700

    nambah

--- PZN.js
+++ PZN.js
@@ -93,16 +93,50 @@ naunx.sayHello("Mamen");
 
 // && CONSTRUCTOR INHERITTANCE  &&&
 
-function Employee (firstName){
-    this.firstName = firstName;
-}
+// function Employee (firstName){
+//     this.firstName = firstName;
+// }
 
-function Manager (firstName, lastName){
-    this.lastName = lastName;
-    Employee.call(this, firstName);
-}
+// function Manager (firstName, lastName){
+//     this.lastName = lastName;
+//     Employee.call(this, firstName);
+// }
 
-const nixau = new Manager("Niko", "noviyanto");
-const naunx = new Manager("anung", "aditya");
-console.log(naunx);
-console.log(nixau);
+// const nixau = new Manager("Niko", "noviyanto");
+// const naunx = new Manager("anung", "aditya");
+// console.log(naunx);
+// console.log(nixau);
+
+// let nama2 = new String("nixooo");
+// console.info(typeof nama2);
+
+// &*& KEKUATAN DARI OBJECT CLASS &*&
+
+// MEMBUAT OBJECT BIASA DI JS. DI DALAM OBJECT NYA, PROPERTY/ATTR MENGGUNAKAN : , DAN DI AKHIR MENGGUNAKAN ,
+// const car = {
+//     name : "papanza",
+//     color : "black"
+// }
+// console.log(car);
+// console.log();
+
+// MEMBUAT OBJECT MENGGUNAKAN CLASS, DI DALAM CLASS-NYA, PROPERTY/ATTR MENGGUNAKAN = , DAN DI AKHIR MENGGUNAKAN ;
+
+class Car{
+    name = "xinia";
+    color = "silver";
+    displacement = 1500;
+}
+const mbem = new Car();
+mbem.name = "CRV"
+mbem.displacement = 2400;
+
+const mbem2 = new Car();
+mbem2.color = "red";
+mbem2.name = "papanjah";
+
+console.log(mbem);
+console.log(mbem2);
+
+
+
